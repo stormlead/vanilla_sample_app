ead888de3b7bb978e0708abde01b391133056bd6
George Kalogeropoulos <george@rent.io>
2013-03-22 17:15:46 -0700

    Resetting tutorial to start of 7.5

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '4040c4562bc4b6bd2110c50b3c02ca88cb3097186776061f4b7778ce56bcb0e8be89d5fa625f4aad3653339287d18521a031c45b9a77ebb070279bad07d5c537'
